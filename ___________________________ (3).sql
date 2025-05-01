adca78a50dbc4e22e4444abb9401cb386e54a45b
wolfcub777 <a0189558@gmail.com>
2025-05-01 13:57:36 +0800

    Add files via upload

---
+++ ___________________________ (3).sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2025-05-01 07:57:06
+-- 伺服器版本： 10.4.32-MariaDB
+-- PHP 版本： 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `停車場巡查使用系統`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `辨識結果`
+--
+
+CREATE TABLE `辨識結果` (
+  `編號` int(11) NOT NULL,
+  `辨識車牌` varchar(20) NOT NULL,
+  `新增時間` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `辨識結果`
+--
+ALTER TABLE `辨識結果`
+  ADD PRIMARY KEY (`編號`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `辨識結果`
+--
+ALTER TABLE `辨識結果`
+  MODIFY `編號` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
